aeab68775e92078dc8b97c2cf2a2f726a382bc32
Alan Platt <aplatt@equalexperts.com>
2018-05-17 15:37:34 +0100

    add simple buildDeploy job

---
+++ vars/buildDeploy.groovy
@@ -0,0 +1,26 @@
+def call(Map config) {
+  node() {
+
+    def appName = config.appName
+    def gitProvider = config.gitProvider
+    def appRepo = config.appRepo
+    def deployNameSpace = config.deployNameSpace
+    def repo = checkout scm
+    def gitVersion = sh(returnStdout: true, script: 'git describe --tags --dirty=.dirty').trim()
+
+    stage('Checkout repo') {
+      git url: "git@${gitProvider}:${appRepo}/${appName}.git"
+    }
+
+    stage('Build') {
+        sh "VERSION=${gitVersion} ./build.sh"
+    }
+
+    stage('Update deployment') {
+      if (env.BRANCH_NAME == 'master') {
+        sh "kubectl apply -f k8s.yaml"
+        sh "kubectl set image deployment/${appName} ${appName}=${appRepo}/${appName}:${gitVersion} -n ${deployNameSpace}"
+      }
+    }
+  }
+}
