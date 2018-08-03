05d8076798bc02cb969228e0274dc1f2481615d3
Giorgio Previtera <gprevitera@equalexperts.com>
2018-08-03 11:37:06 +0200

    Enable timestamps

--- vars/buildDeploy.groovy
+++ vars/buildDeploy.groovy
@@ -1,25 +1,27 @@
 def call(Map config) {
   node() {
 
-    def appName = config.appName
-    def gitProvider = config.gitProvider
-    def appRepo = config.appRepo
-    def deployNameSpace = config.deployNameSpace
-    def repo = checkout scm
-    def gitVersion = sh(returnStdout: true, script: 'git describe --tags --dirty=.dirty').trim()
+    timestamps {
+      def appName = config.appName
+      def gitProvider = config.gitProvider
+      def appRepo = config.appRepo
+      def deployNameSpace = config.deployNameSpace
+      def repo = checkout scm
+      def gitVersion = sh(returnStdout: true, script: 'git describe --tags --dirty=.dirty').trim()
 
-    stage('Checkout repo') {
-      git url: "git@${gitProvider}:${appRepo}/${appName}.git", branch: env.BRANCH_NAME
-    }
+      stage('Checkout repo') {
+        git url: "git@${gitProvider}:${appRepo}/${appName}.git", branch: env.BRANCH_NAME
+      }
 
-    stage('Build') {
-        sh "VERSION=${gitVersion} ./build.sh"
-    }
+      stage('Build') {
+          sh "VERSION=${gitVersion} ./build.sh"
+      }
 
-    stage('Update deployment') {
-      if (env.BRANCH_NAME == 'master') {
-        sh "kubectl apply -f k8s.yaml"
-        sh "kubectl set image deployment/${appName} ${appName}=${appRepo}/${appName}:${gitVersion} -n ${deployNameSpace}"
+      stage('Update deployment') {
+        if (env.BRANCH_NAME == 'master') {
+          sh "kubectl apply -f k8s.yaml"
+          sh "kubectl set image deployment/${appName} ${appName}=${appRepo}/${appName}:${gitVersion} -n ${deployNameSpace}"
+        }
       }
     }
   }
--- vars/ecrBuildPushDeploy.groovy
+++ vars/ecrBuildPushDeploy.groovy
@@ -1,37 +1,39 @@
 def call(Map config) {
   node() {
 
-    def appName = config.appName
-    def gitProvider = config.gitProvider
-    def appRepo = config.appRepo
-    def deployNameSpace = config.deployNameSpace
-    def ecrRepo = config.ecrRepo
-    def repo = checkout scm
-    def gitVersion = sh(returnStdout: true, script: 'git describe --tags --dirty=.dirty').trim()
+    timestamps {
+      def appName = config.appName
+      def gitProvider = config.gitProvider
+      def appRepo = config.appRepo
+      def deployNameSpace = config.deployNameSpace
+      def ecrRepo = config.ecrRepo
+      def repo = checkout scm
+      def gitVersion = sh(returnStdout: true, script: 'git describe --tags --dirty=.dirty').trim()
 
-    stage('Checkout repo') {
-      git url: "git@${gitProvider}:${appRepo}/${appName}.git", branch: env.BRANCH_NAME
-    }
-
-    stage('ECR login') {
-      sh "`aws ecr get-login --no-include-email`"
-    }
-
-    stage('Build image') {
-      sh "./build.sh ${ecrRepo}/${appName} ${gitVersion}"
-    }
-
-    stage('Push image') {
-      if (env.BRANCH_NAME == 'master') {
-        sh "docker push ${ecrRepo}/${appName}:${gitVersion}"
-        sh "docker push ${ecrRepo}/${appName}:latest"
+      stage('Checkout repo') {
+        git url: "git@${gitProvider}:${appRepo}/${appName}.git", branch: env.BRANCH_NAME
       }
-    }
 
-    stage('Update kubernetes') {
-      if (env.BRANCH_NAME == 'master') {
-        sh "kubectl apply -f k8s.yaml"
-        sh "kubectl set image deployment/${appName} ${appName}=${ecrRepo}/${appName}:${gitVersion} -n ${deployNameSpace}"
+      stage('ECR login') {
+        sh "`aws ecr get-login --no-include-email`"
+      }
+
+      stage('Build image') {
+        sh "./build.sh ${ecrRepo}/${appName} ${gitVersion}"
+      }
+
+      stage('Push image') {
+        if (env.BRANCH_NAME == 'master') {
+          sh "docker push ${ecrRepo}/${appName}:${gitVersion}"
+          sh "docker push ${ecrRepo}/${appName}:latest"
+        }
+      }
+
+      stage('Update kubernetes') {
+        if (env.BRANCH_NAME == 'master') {
+          sh "kubectl apply -f k8s.yaml"
+          sh "kubectl set image deployment/${appName} ${appName}=${ecrRepo}/${appName}:${gitVersion} -n ${deployNameSpace}"
+        }
       }
     }
   }
