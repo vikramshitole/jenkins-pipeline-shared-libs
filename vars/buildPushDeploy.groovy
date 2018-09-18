7c4e73d3324f560ba751a3a84b3ad950b24eef28
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2018-09-19 00:31:43 +0100

    Fix incorrect placement of isOnAzure()

--- vars/buildPushDeploy.groovy
+++ vars/buildPushDeploy.groovy
@@ -1,14 +1,14 @@
-def call(Map config) {
+def isOnAzure() {
+  def metadataUrl = 'http://169.254.169.254/metadata/instance?api-version=2017-08-01'
+  def response = httpRequest(
+                      url: metadataUrl,
+                      customHeaders: [[name: 'Metadata', value: 'true']],
+                      validResponseCodes: "100:499"
+                  )
+  return response.status == 200
+}
 
-  def isOnAzure() {
-    def metadataUrl = 'http://169.254.169.254/metadata/instance?api-version=2017-08-01'
-    def response = httpRequest(
-                        url: metadataUrl,
-                        customHeaders: [[name: 'Metadata', value: 'true']],
-                        validResponseCodes: "100:499"
-                    )
-    return response.status == 200
-  }
+def call(Map config) {
 
   if (isOnAzure()) {
     config.registryAddress = config.registryAddresses['acr']
